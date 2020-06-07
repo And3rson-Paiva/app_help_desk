fd7c81df8d556af7afdb9c3908b4890fd998cc9c
Anderson Acosta <and3rs0npa1v4@gmail.com>
2020-06-07 17:23:18 -0300

    variável de verificação e percorrendo lista de usuários
    organização do metodo POST na parte de cima do código

--- valida_login.php
+++ valida_login.php
@@ -1,13 +1,30 @@
 <?php
-    //Trocar pela requisição de usuários do banco de dados
+    //Variável que verifica se a autenticação foi realizada
+    $usuario_autenticado = false;
+
+    //chamada ao name relacionado ao input html
+    $_POST['email'];
+    $_POST['senha'];
+
+    //Usuários do sistema, trocar pela requisição de usuários do banco de dados
     $usuarios_app = array(
         array('email' => 'adm@teste.com.br', 'senha' => '123456'),
         array('email' => 'user@teste.com.br', 'senha' => 'abcd'),
     );
 
+    //percorrendo usuarios
+    foreach($usuarios_app as $user){
+        if($user['email'] == $_POST['email'] && $user['senha'] == $_POST['senha']){
+            $usuario_autenticado = true;
+        }
+    }
 
-    //chamada ao name relacionado ao input html
-    $_POST['email'];
-    $_POST['senha'];
+    if($usuario_autenticado){
+       echo 'Usuário autenticado'; 
+    }else{
+        echo 'Erro na autenticação do usuário';
+    }
+
+    
 
 ?>
