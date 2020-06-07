744da2e913ac23a4d5459595d558cdee890b8288
Anderson Acosta <and3rs0npa1v4@gmail.com>
2020-06-07 17:05:58 -0300

    adicionado array de teste de usuarios

--- valida_login.php
+++ valida_login.php
@@ -1,5 +1,12 @@
 <?php
+    //Trocar pela requisição de usuários do banco de dados
+    $usuarios_app = array(
+        array('email' => 'adm@teste.com.br', 'senha' => '123456'),
+        array('email' => 'user@teste.com.br', 'senha' => 'abcd'),
+    );
 
+
+    //chamada ao name relacionado ao input html
     $_POST['email'];
     $_POST['senha'];
 
